71ec1d11af83d7db52716343173cb0007e398665
g21turbo <dg21stew@gmail.com>
2023-11-26 20:28:49 -0800

    forgot default email

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,10 @@ public class Main {
 
         System.out.println("Customer Name: " + bob.getName() + "; Credit Limit: $" + bob.getCreditLimit());
 
+        Customer default1 = new Customer();
+
+        System.out.println("Customer Name: " + default1.getName() + "; Email: " + default1.getEmailAddress() + "; Credit Limit: $" + default1.getCreditLimit());
+
+
     }
 }
